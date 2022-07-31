bb37e619072c56e491f3184cca00914388e04d37
Uirá de Souza <uira182@hotmail.com>
2022-07-31 17:44:11 -0300

    Adicionei o componente toast.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,7 +10,7 @@ export class Tab1Page {
   handlerMessage = '';
   roleMessage = '';
 
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController, public toastController: ToastController) {}
   async presentAlert() {
     const alert = await this.alertController.create({
       header: 'Alert!',
@@ -19,15 +19,25 @@ export class Tab1Page {
         {
           text: 'Cancelar',
           role: 'cancel',
-          handler: (blah) => { console.log('Confirm Canel: blah'); }
+          handler: (blah) => { console.log('Confirm Cancel: blah'); }
         },
         {
           text: 'Sim, Favoritar!',
-          handler: () => { console.log('Confirm Okay.'); }
+          handler: () => { console.log('Confirm Okay.'); this.presentToast(); }
         }
       ]
     });
 
     await alert.present();
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Filme adicionado aos favoritos.',
+      duration: 2000,
+      position: 'top',
+      color:'success'
+    });
+    toast.present();
   }
 }
