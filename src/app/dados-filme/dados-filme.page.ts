d47c7ac752cbb86e68671a6305cc53644a2c47ae
Uirá de Souza <uira182@hotmail.com>
2022-08-01 19:50:03 -0300

    Adicionei as descrições dos filmes e series e agora estou adicionando os trailers.

--- src/app/dados-filme/dados-filme.page.ts
+++ src/app/dados-filme/dados-filme.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IFilmeApi } from '../models/IFilmeAPI.model';
+import { IListaTrailer, ITrailer } from '../models/ITrailer.model';
 import { DadosService } from '../services/dados.service';
+import { TrailerService } from '../services/trailer.service';
 
 @Component({
   selector: 'app-dados-filme',
@@ -10,14 +12,20 @@ import { DadosService } from '../services/dados.service';
 export class DadosFilmePage implements OnInit {
 
   filme: IFilmeApi;
+  trailer: IListaTrailer;
   generos: string[] = [];
 
-  constructor(public dadosService: DadosService) { }
-
-  ngOnInit() {
+  constructor(public dadosService: DadosService, public trailerService: TrailerService) {
     this.filme = this.dadosService.pegarDados('filme');
     this.generos = this.dadosService.pegarDados('generos');
+    this.trailerService.buscarTrailerFilme(this.filme.id).subscribe(dados=>{
+      console.log(dados);
+      this.trailer = dados;
+    });
     console.log('Filme Enviado', this.filme);
+  }
+
+  ngOnInit() {
   }
 
 }
---
+++ src/app/services/trailer.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { IListaTrailer } from '../models/ITrailer.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrailerService {
+
+  lingua = 'pt-BR';
+
+  private apiURL = 'https://api.themoviedb.org/3/';
+  private key = '?api_key=0961a07b3607c784ef87c0974cea8930';
+
+  constructor(private http: HttpClient, public toastController: ToastController) { }
+
+  buscarTrailerFilme(id: any): Observable<IListaTrailer>{
+    const url = `${this.apiURL}/movie/${id}/videos${this.key}&language=${this.lingua}`;
+
+    return this.http.get<IListaTrailer>(url).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
+  }
+
+  buscarTrailerSerie(id: number): Observable<IListaTrailer>{
+    const url = `${this.apiURL}/movie/${id}/videos${this.key}&language=${this.lingua}`;
+
+    return this.http.get<IListaTrailer>(url).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
+  }
+
+  async exibirErro(erro) {
+    const toast = await this.toastController.create({
+      message: 'Erro ao consultar a API!',
+      duration: 2000,
+      color: 'danger',
+      position: 'middle'
+    });
+    toast.present();
+    return null;
+  }
+}
