4ddb7b5bc9136924adb7f2e0e17bce40b85e71f5
Uirá de Souza <uira182@hotmail.com>
2022-08-01 00:16:51 -0300

    Adicionei a API do the movie DB e arrumamos toda sua interface com os serviços e models.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -111,6 +111,8 @@ export class Tab1Page implements OnInit{
         dados.genres.forEach(genero => {
           this.generos[genero.id] = genero.name;
         });
+
+        this.dadosService.guardarDados('generos', this.generos);
       });
   }
 }
